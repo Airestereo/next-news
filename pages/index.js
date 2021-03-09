711b5c16cddaf3e57f777b4b453997adccd6023c
Carlos Ernesto Vazquez <airestereo@outlook.com>
2021-03-08 21:44:51 -0800

    video

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { Toolbar } from '../components/toolbar';
 //import dynamic from 'next/dynamic'
+import ReactPlayer from 'react-player';
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 
@@ -9,15 +11,27 @@ export default function Home() {
 
   <div className='page-container' >
 
+
+
 <Toolbar/>
+
+<div className={styles.home}>
+
+
+      <ReactPlayer url="https://www.youtube.com/watch?v=xcLOVyKaMgk"/>
+        
+
+    </div>
 
 
 <div className={styles.main}>
+
+
+
 <h1>Next.Js Network App</h1>
 <h3>Your One Stop Shop For Entertainment and The Latest News Articles</h3>
 </div>
 </div>
-
 
 
 );
