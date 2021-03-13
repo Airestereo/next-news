aab734df7512ae578fd860bd5f316558064b594f
Carlos Ernesto Vazquez <airestereo@outlook.com>
2021-03-13 14:11:08 -0800

    some

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
 
 <div className={styles.home}>
 
-<ReactPlayer url="https://www.twitch.tv/housemusiclivestreameu"/> 
+<ReactPlayer url="https://www.twitch.tv/fodlivestreams2020"/> 
 
 </div>
 
