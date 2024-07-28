1549eac4a3b12928210909a015bb7bdab7fbe53d
Airestereo <airestereo@outlook.com>
2024-07-27 20:52:14 -0700

    Audit Fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     images: {
-      domains: ['example.com', 'https://newsapi.org', 'media.nbcnewyork.com'],
+      domains: ['example.com', 'https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page', 'media.nbcnewyork.com'],
     },
   };
+  
   
